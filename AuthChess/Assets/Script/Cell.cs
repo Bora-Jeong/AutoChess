59a7118bb554acd3e3fdda83015ca2b36d9bb8db
Bora-Jeong <50454596+Bora-Jeong@users.noreply.github.com>
2020-10-19 04:40:05 +0900

    init

---
+++ AuthChess/Assets/Script/Cell.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cell : MonoBehaviour
+{
+    public enum Type
+    {
+        Inventory,
+        MyField,
+        EnemyField
+    }
+
+    [SerializeField]
+    private Type _type;
+
+    public Type type => _type;
+    public bool isOccupied { get; set; }
+}
---
+++ AuthChess/Assets/Script/Constants.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Constants
+{
+    public static readonly float prepareTimeSpan = 30f;
+    public static readonly float waitTimeSpan = 5f;
+    public static readonly float battleTimeSpan = 30f;
+}
